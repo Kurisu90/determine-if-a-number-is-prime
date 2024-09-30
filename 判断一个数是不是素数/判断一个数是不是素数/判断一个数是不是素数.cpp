e03222bc9619ac9e3512a8dd83da0697bff5d7cc
Makise Kurisu <3887277988@qq.com>
2024-09-30 17:05:38 +0800

    Add files via upload

---
+++ 判断一个数是不是素数/判断一个数是不是素数/判断一个数是不是素数.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<math.h>
+int GET() {
+	int a;
+	printf("Please enter a number:");
+	scanf_s("%d", &a);
+	return a;
+}//读取输入的一个数
+int main() {
+	int x = GET();//输入的数
+	int i=2;
+	while (x%i!=0) {
+		i++;
+	}
+	if (i == x) {
+		printf("Yes.");
+	}
+	else {
+		printf("No.");
+	}
+}
+
+
